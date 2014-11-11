3f6c6bcf1fb6bcbd3c0cd7ddab63e3a109055d33
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-11-11 23:17:21 +0100

    Removed HttpClientContext, added very ugly hack to prevent random crashes of php build in web server updated php-matcher to version 1.1.*

--- features/bootstrap/TutuContext.php
+++ features/bootstrap/TutuContext.php
@@ -1,14 +1,20 @@
 <?php
 
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\Testwork\Tester\Result\TestResult;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\ProcessBuilder;
 
 class TutuContext extends RawMinkContext implements SnippetAcceptingContext
 {
+    const MAX_PHP_SERVER_RESTARTS = 5;
+
     /**
      * @var Process
      */
@@ -20,6 +26,11 @@ class TutuContext extends RawMinkContext implements SnippetAcceptingContext
     private $tutuPort;
 
     /**
+     * @var string;
+     */
+    private $tutuHost;
+
+    /**
      * @var string
      */
     private $webPath;
@@ -29,12 +40,18 @@ class TutuContext extends RawMinkContext implements SnippetAcceptingContext
      */
     private $workDir;
 
+    /**
+     * @var int
+     */
+    private $httpCallAttemptsPerScenario = 0;
+
     public function __construct($webPath)
     {
         if (!file_exists($webPath)) {
             throw new \InvalidArgumentException(sprintf("Path %s does not exist", $webPath));
         }
         $this->webPath = $webPath;
+        $this->headersToRemove = [];
     }
 
     /**
@@ -53,6 +70,22 @@ class TutuContext extends RawMinkContext implements SnippetAcceptingContext
         $fs->mkdir($this->workDir . '/resources');
         $fs->mkdir($this->workDir . '/config');
         $fs->dumpFile($this->workDir . '/config/responses.yml', '');
+    }
+
+    /** @AfterScenario */
+    public function printLastResponseWhenScenarioFail(AfterScenarioScope $scope)
+    {
+        if ($scope->getTestResult()->getResultCode() === TestResult::FAILED) {
+            echo "Last response:\n";
+            echo "Code " . $this->getSession()->getStatusCode() . "\n";
+            $this->printLastResponse();
+        }
+    }
+
+    /** @AfterScenario */
+    public function resetHttpCallAttempts()
+    {
+        $this->httpCallAttemptsPerScenario = 0;
     }
 
     /**
@@ -129,11 +162,12 @@ class TutuContext extends RawMinkContext implements SnippetAcceptingContext
     public function tutuIsRunningOnHostAtPort($host, $port)
     {
         $this->tutuPort = $port;
+        $this->tutuHost = $host;
         $this->killEveryProcessRunningOnTuTuPort();
         $builder = new ProcessBuilder([
             PHP_BINARY,
             '-S',
-            sprintf('%s:%s', $host, $port)
+            sprintf('%s:%s > /Users/norzechowicz/Workspace/PHP/coduo/TuTu/log.txt', $host, $port)
         ]);
 
         if (file_exists($this->workDir . '/config/config.yml')) {
@@ -144,8 +178,164 @@ class TutuContext extends RawMinkContext implements SnippetAcceptingContext
         $builder->setEnv('tutu_resources', $this->workDir . '/resources');
         $builder->setWorkingDirectory($this->webPath);
         $builder->setTimeout(null);
+
         $this->tutuProcess = $builder->getProcess();
         $this->tutuProcess->start();
         sleep(1);
+    }
+
+    /**
+     * @Then print last response
+     */
+    public function printLastResponse()
+    {
+        $content = $this->getSession()->getDriver()->getContent();
+
+        echo "\n\033[36m|  " . strtr($content, array("\n" => "\n|  ")) . "\033[0m\n\n";
+    }
+
+    /**
+     * @Then response status code should be :expectedStatus
+     */
+    public function responseStatusCodeShouldBe($expectedStatus)
+    {
+        $status = $this->getSession()->getStatusCode();
+        if ($status !== $expectedStatus) {
+            throw new \RuntimeException(sprintf("Status %d is not equal to %d.", $status, (int) $expectedStatus));
+        }
+    }
+
+    /**
+     * @Then the response content should be equal to:
+     */
+    public function theResponseContentShouldBeEqualTo(PyStringNode $expectedContent)
+    {
+        $content = $this->getSession()->getDriver()->getContent();
+        if ((string) $content !== (string) $expectedContent) {
+            throw new \RuntimeException("Content is different than expected.");
+        }
+    }
+
+    /**
+     * @Then the response content should match expression:
+     */
+    public function theResponseContentShouldMatchExpression(PyStringNode $pattern)
+    {
+        $content = $this->getSession()->getDriver()->getContent();
+        expect($content)->toMatch('/'.$pattern.'/');
+    }
+
+    /**
+     * @Then the response content should be empty
+     */
+    public function theResponseContentShouldBeEmpty()
+    {
+        $content = $this->getSession()->getDriver()->getContent();
+        if (!empty($content)) {
+            throw new \RuntimeException("Content is not empty.");
+        }
+    }
+
+    /**
+     * @Then response should have following hedaers:
+     */
+    public function responseShouldHaveFollowingHedaers(TableNode $responseHeaders)
+    {
+        $headers = $this->getSession()->getResponseHeaders();
+
+        foreach ($responseHeaders->getHash() as $header) {
+            if (!array_key_exists($header['Name'], $headers)) {
+                throw new \RuntimeException(sprintf("There is no \"%s\" header in response.", $header['Name']));
+            }
+
+            if ($headers[$header['Name']][0] !== $header['Value']) {
+                throw new \RuntimeException(sprintf(
+                    "Header \"%s\" value \"%s\" is not equal to \"%s\".",
+                    $header['Name'],
+                    $headers[$header['Name']][0],
+                    $header['Value']
+                ));
+            }
+        }
+    }
+
+
+    /**
+     * @When http client sends :method request on :url
+     */
+    public function httpClientSendRequestOn($method, $url)
+    {
+        $this->makeHttpCall($method, $url);
+    }
+
+    /**
+     * @When http client sends :method request on :url with following parameters:
+     */
+    public function httpClientSendPostRequestOnWithFollowingParameters($method, $url, TableNode $parametersTable)
+    {
+        $parameters = [];
+        foreach ($parametersTable->getHash() as $parameterData) {
+            $parameters[$parameterData['Parameter']] = $parameterData['Value'];
+        }
+
+        $this->makeHttpCall($method, $url, $parameters);
+    }
+
+    /**
+     * @When http client sends :method request on :url with following headers
+     */
+    public function httpClientSendGetRequestOnWithFollowingHeaders($method, $url, TableNode $headersTable)
+    {
+        $session = $this->getSession();
+        $client = $session->getDriver()->getClient();
+        $headers = [];
+        foreach ($headersTable->getHash() as $headerData) {
+            $client->setHeader($headerData['Header'], $headerData['Value']);
+            $headers[] = $headerData['Header'];
+        }
+
+        $this->makeHttpCall($method, $url, [], $headers);
+    }
+
+    /**
+     * @When http client sends :method request on :url with body
+     */
+    public function httpClientSendGetRequestOnWithBody($method, $url, PyStringNode $body)
+    {
+        $this->makeHttpCall($method, $url, [], [], (string) $body);
+    }
+
+    /**
+     * @param $method
+     * @param $url
+     * @param array $parameters
+     * @param null $body
+     */
+    private function makeHttpCall($method, $url, array $parameters = [], array $headers = [], $body = null)
+    {
+        $session = $this->getSession();
+        $client = $session->getDriver()->getClient();
+        $client->followRedirects(false);
+        try {
+            $client->request(
+                $method,
+                $url,
+                $parameters, // parameters
+                [], // files
+                $headers, // $_SERVER
+                $body
+            );
+        } catch (RequestException $exception) {
+            if (strpos($exception->getMessage(), 'cURL error 7') !== false) {
+                if ($this->httpCallAttemptsPerScenario < self::MAX_PHP_SERVER_RESTARTS) {
+                    $this->httpCallAttemptsPerScenario++;
+                    $this->tutuIsRunningOnHostAtPort($this->tutuHost, $this->tutuPort);
+                    $this->makeHttpCall($method, $url, $parameters, $headers, $body);
+                    return ;
+                }
+            }
+
+            throw $exception;
+        }
     }
 }
